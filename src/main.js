c64702621ee5f1609d08f6892a20fbefadff2f29
Huntly Cameron <huntly.cameron@gmail.com>
2022-11-18 15:08:20 +0000

    ... should really edit properly and not through gh

--- src/main.js
+++ src/main.js
@@ -20,11 +20,6 @@ export default function (Vue, { router, head, isClient }) {
     rel: "me",
     href: "https://mastodon.social/@huntlyc"
   });
-  
-  head.link.push({
-    rel: 'icon',
-    href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>'
-  });
 
   head.script.push({
       src: 'https://identity.netlify.com/v1/netlify-identity-widget.js',
